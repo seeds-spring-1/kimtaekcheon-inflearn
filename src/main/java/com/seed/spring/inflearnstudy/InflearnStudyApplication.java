a532ed135064e9656b36348fbf8fb49f24d5505b
10cheon00 <10cheon00@gmail.com>
2023-11-01 13:52:44 +0900

    섹션1: 라이브러리 살펴보기

---
+++ src/main/java/com/seed/spring/inflearnstudy/InflearnStudyApplication.java
@@ -0,0 +1,13 @@
+package com.seed.spring.inflearnstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seed/spring/inflearnstudy/InflearnStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seed.spring.inflearnstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
