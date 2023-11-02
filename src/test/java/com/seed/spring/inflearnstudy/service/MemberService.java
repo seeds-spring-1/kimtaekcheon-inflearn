5eac853b839c3ba677de42c6d2a6d9041bdaff97
Kim Taekcheon <10cheon00@gmail.com>
2023-11-02 13:28:00 +0900

    섹션3: 회원 서비스 개발

---
+++ src/test/java/com/seed/spring/inflearnstudy/service/MemberService.java
@@ -0,0 +1,34 @@
+package com.seed.spring.inflearnstudy.service;
+
+import com.seed.spring.inflearnstudy.domain.Member;
+import com.seed.spring.inflearnstudy.repository.MemberRepository;
+import com.seed.spring.inflearnstudy.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        // unique name check
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
